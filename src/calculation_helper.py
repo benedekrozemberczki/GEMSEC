52e52721e20a6c5ebb499a21ddd49295846eec49
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-05-25 14:55:56 +0100

    Update calculation_helper.py

--- src/calculation_helper.py
+++ src/calculation_helper.py
@@ -164,44 +164,44 @@ class RandomWalker:
 
 class SecondOrderRandomWalker:
     def __init__(self, nx_G, is_directed, p, q):
-	    self.G = nx_G
+    self.G = nx_G
         self.nodes = nx.nodes(self.G)
         print("Edge weighting.\n")
         for edge in tqdm(self.G.edges()):
             self.G[edge[0]][edge[1]]['weight'] = 1.0
             self.G[edge[1]][edge[0]]['weight'] = 1.0
-	    self.is_directed = is_directed
-	    self.p = p
-	    self.q = q
+    self.is_directed = is_directed
+    self.p = p
+    self.q = q
 
     def node2vec_walk(self, walk_length, start_node):
         '''
-	    Simulate a random walk starting from start node.
-	    '''
-	    G = self.G
-	    alias_nodes = self.alias_nodes
-	    alias_edges = self.alias_edges
+    Simulate a random walk starting from start node.
+    '''
+    G = self.G
+    alias_nodes = self.alias_nodes
+    alias_edges = self.alias_edges
 
-	    walk = [start_node]
+    walk = [start_node]
 
-	    while len(walk) < walk_length:
-	        cur = walk[-1]
-	        cur_nbrs = sorted(G.neighbors(cur))
-	        if len(cur_nbrs) > 0:
-	            if len(walk) == 1:
-		            walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
-		        else:
-		            prev = walk[-2]
-		            next = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0], alias_edges[(prev, cur)][1])]
-		            walk.append(next)
-	        else:
-		        break
+    while len(walk) < walk_length:
+        cur = walk[-1]
+        cur_nbrs = sorted(G.neighbors(cur))
+        if len(cur_nbrs) > 0:
+            if len(walk) == 1:
+            walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
+        else:
+            prev = walk[-2]
+            next = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0], alias_edges[(prev, cur)][1])]
+            walk.append(next)
+        else:
+        break
 
-	    return walk
+    return walk
 
     def count_frequency_values(self, walks):
         """ 
-       	Calculate the co-occurence frequencies.
+           Calculate the co-occurence frequencies.
         """
         raw_counts = [node for walk in walks for node in walk]
         counts = Counter(raw_counts)
@@ -209,82 +209,82 @@ class SecondOrderRandomWalker:
         return self.degrees 
 
     def simulate_walks(self, num_walks, walk_length):
-	    """
+    '''
         Repeatedly simulate random walks from each node.
-	    """"
-	    G = self.G
-	    walks = []
-	    nodes = list(G.nodes())
-	    for walk_iter in range(num_walks):
+    '''
+    G = self.G
+    walks = []
+    nodes = list(G.nodes())
+    for walk_iter in range(num_walks):
             print(" ")
             print("Random walk series " + str(walk_iter+1) + ". initiated.")
             print(" ")
-	        random.shuffle(nodes)
-	        for node in tqdm(nodes):
-		        walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
+        random.shuffle(nodes)
+        for node in tqdm(nodes):
+        walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
 
-	    return walks, self.count_frequency_values(walks)
+    return walks, self.count_frequency_values(walks)
 
     def get_alias_edge(self, src, dst):
-        """
-	    Get the alias edge setup lists for a given edge.
-	    """
-	    G = self.G
-	    p = self.p
-	    q = self.q
+        '''
+    Get the alias edge setup lists for a given edge.
+    '''
+    G = self.G
+    p = self.p
+    q = self.q
 
-	    unnormalized_probs = []
-	    for dst_nbr in sorted(G.neighbors(dst)):
-	        if dst_nbr == src:
-	            unnormalized_probs.append(G[dst][dst_nbr]['weight']/p)
-	        elif G.has_edge(dst_nbr, src):
-		        unnormalized_probs.append(G[dst][dst_nbr]['weight'])
-	        else:
-		        unnormalized_probs.append(G[dst][dst_nbr]['weight']/q)
-	        norm_const = sum(unnormalized_probs)
-	        normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+    unnormalized_probs = []
+    for dst_nbr in sorted(G.neighbors(dst)):
+        if dst_nbr == src:
+            unnormalized_probs.append(G[dst][dst_nbr]['weight']/p)
+        elif G.has_edge(dst_nbr, src):
+        unnormalized_probs.append(G[dst][dst_nbr]['weight'])
+        else:
+        unnormalized_probs.append(G[dst][dst_nbr]['weight']/q)
+    norm_const = sum(unnormalized_probs)
+    normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
 
-	    return alias_setup(normalized_probs)
+    return alias_setup(normalized_probs)
 
     def preprocess_transition_probs(self):
-	"""
-	Preprocessing of transition probabilities for guiding the random walks.
-	"""
-	G = self.G
-	is_directed = self.is_directed
+    '''
+    Preprocessing of transition probabilities for guiding the random walks.
+    '''
+    G = self.G
+    is_directed = self.is_directed
 
-	alias_nodes = {}
+    alias_nodes = {}
         print("")
         print("Preprocesing.\n")
-	for node in tqdm(G.nodes()):
-	    unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
-	    norm_const = sum(unnormalized_probs)
-	    normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
-	    alias_nodes[node] = alias_setup(normalized_probs)
+    for node in tqdm(G.nodes()):
+        unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
+        norm_const = sum(unnormalized_probs)
+        normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+        alias_nodes[node] = alias_setup(normalized_probs)
 
-	alias_edges = {}
-	triads = {}
+    alias_edges = {}
+    triads = {}
 
-	if is_directed:
-	    for edge in G.edges():
-	        alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-	else:
-	    for edge in tqdm(G.edges()):
-	        alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-		alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
+    if is_directed:
+        for edge in G.edges():
+            alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
+    else:
+        for edge in tqdm(G.edges()):
+            alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
+        alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
 
-	self.alias_nodes = alias_nodes
-	self.alias_edges = alias_edges
+    self.alias_nodes = alias_nodes
+    self.alias_edges = alias_edges
 
-	return
+    return
 
 
 def alias_setup(probs):
-    """
+    '''
     Compute utility lists for non-uniform sampling from discrete distributions.
     Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
     for details
-    """
+    '''
     K = len(probs)
     q = np.zeros(K)
     J = np.zeros(K, dtype=np.int)
@@ -293,28 +293,28 @@ def alias_setup(probs):
 
     for kk, prob in enumerate(probs):
         q[kk] = K*prob
-	if q[kk] < 1.0:
-	    smaller.append(kk)
-	else:
-	    larger.append(kk)
+    if q[kk] < 1.0:
+        smaller.append(kk)
+    else:
+        larger.append(kk)
 
     while len(smaller) > 0 and len(larger) > 0:
-	small = smaller.pop()
-	large = larger.pop()
+    small = smaller.pop()
+    large = larger.pop()
 
-	J[small] = large
-	q[large] = q[large] + q[small] - 1.0
-	if q[large] < 1.0:
-	    smaller.append(large)
-	else:
-	    larger.append(large)
+    J[small] = large
+    q[large] = q[large] + q[small] - 1.0
+    if q[large] < 1.0:
+        smaller.append(large)
+    else:
+        larger.append(large)
 
     return J, q
 
 def alias_draw(J, q):
-    """
+    '''
     Draw sample from a non-uniform discrete distribution using alias sampling.
-    """
+    '''
     K = len(J)
 
     kk = int(np.floor(np.random.rand()*K))
