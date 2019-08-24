c3a44c272652e663f8bef437d6e6ae5da914ec59
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-08-23 23:59:55 -0400

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,12 +1,10 @@
 import argparse
 
 def parameter_parser():
-
     """
     A method to parse up command line parameters. By default it gives an embedding of the Facebook politicians network.
     The default hyperparameters give a good quality representation and good candidate cluster means without grid search.
     """
-
     parser = argparse.ArgumentParser(description = "Run GEMSEC.")
 
     #------------------------------------------------------------------
